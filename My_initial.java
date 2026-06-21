a90a6f725d9f75a0fd87f856ff0aa0ea5138be2f
sivateja-creator <chowdaryshiva779@gmail.com>
2026-06-21 09:36:02 +0530

    JAVA

---
+++ My_initial.java
@@ -0,0 +1,17 @@
+public class My_initial {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 8; i++) {
+            for (int j = 1; j <= 7; j++) {
+                if (j == 1 ||
+                    i == 1 && j < 7 ||
+                    i == 4 && j < 7 ||
+                    j == 7 && i > 1 && i < 4) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern.java
@@ -0,0 +1,16 @@
+public class Pattern
+{
+    public static void main(String[] args) {
+        for (int i = 1; i < 8; i++) {
+            for (int j = 1; j < 8; j++) {
+                if(){
+                    System.out.print("* ");
+                }
+                else{
+                    System.out.println("");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
