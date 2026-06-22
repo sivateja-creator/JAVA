7acd986bbeb9b3e5d4e4ae3e22e4ac575a0a7299
sivateja-creator <chowdaryshiva779@gmail.com>
2026-06-22 14:41:04 +0530

    JAVA

---
+++ File_Handling/A.java
@@ -0,0 +1,15 @@
+import java.io.File;
+public class A
+{
+    public static void main(String[] args) 
+    {
+        try {
+        File Data = new File("Sample.java");
+        Data.createNewFile();    
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        
+    }    
+}
+//File Creation
