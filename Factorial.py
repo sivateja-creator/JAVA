8a3b927513814dfe908a7420f19c41c523d8d29e
Padibandla SivaTeja <chowdaryshiva779@gmail.com>
2026-07-17 22:56:39 +0530

    Create Factorial.py

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+# Factorial
+def factorial(n):
+    fact = 1
+    for i in range(1, n + 1):
+        fact = fact * i
+    return fact
+
+n = int(input("Enter a value: "))
+result = factorial(n)
+print(result)
