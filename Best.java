06154f77b96cd63895c709d0211839e20f1e2c65
Padibandla SivaTeja <chowdaryshiva779@gmail.com>
2026-07-20 07:49:33 +0530

    Add Best.java to read and print a name

---
+++ Best.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class Best{
+  public static void main(String[] args)
+  {
+    System.out.print("Enter a name:");
+Scanner sc=new Scanner(System.in);
+    String name=sc.nextLine();
+    System.out.println("Name:"+name);
+    sc.close();
+  }
+}
